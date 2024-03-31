e326fecc7feadfb9d015609dde2064067531c95e
Filip-Mistyuk <mistukfilipp@gmail.com>
2024-03-31 03:24:55 -0700

    .

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', get_rooms_list, name='rooms'),
     path('<int:pk>/', get_room_details, name='room'),
+    path('booking_details/<int:pk>/', booking_details, name='booking-detail'),
+    path('booking/', booking_form, name='booking-form'),
 ]
