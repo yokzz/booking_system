562c32ce727e7a9ea6be519f41cab7238f969316
Zhelizko Daniel <yokzz.programming@gmail.com>
2024-03-31 13:37:47 +0300

    added booking_details, booking-form(templates)  and views

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 from booking.models import *
 
 def get_rooms_list(request):
@@ -14,3 +14,32 @@ def get_room_details(request, pk):
                'room': room,
     }
     return render(request, 'booking/room_details.html', context)
+
+def get_booking_details(request, pk):
+    booking = Booking.objects.get(id=pk)
+    context = {
+                'booking': booking,
+    }
+    return render(request, 'booking/booking_details.html', context)
+
+def get_booking_form(request):
+    if request.method == "GET":
+        return render(request, "booking/booking_form.html")
+    else:
+        room_number = request.POST.get("room_number")
+        start_time = request.POST.get("start_time")
+        end_time = request.POST.get("end_time")
+        
+        # try:
+        room = Room.objects.get(number=room_number)
+        # except ValueError:
+        #     return HttpResponse("Wrong room number", status=404)
+        # except Room.DoesNotExist:
+        #     return HttpResponse("Room doesn't exist", status=404)
+        booking = Booking.objects.create(
+            user=request.user,
+            room=room,
+            start_time=start_time,
+            end_time=end_time
+        )
+        return redirect("booking-detail", pk=booking.id)
